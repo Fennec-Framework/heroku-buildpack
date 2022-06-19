ab41d07fbc91d3ab12db6d067546b1169727b312
Akram <akram.chorfi@stud.uni-hannover.de>
2022-06-19 19:33:55 +0200

    add server check

--- fennec_heroku_example/bin/fennec_heroku_example.dart
+++ fennec_heroku_example/bin/fennec_heroku_example.dart
@@ -18,4 +18,12 @@ class Controller {
   void heltchCheck(Request request, Response response) {
     response.send('your healtcheck is successful');
   }
+
+  @Route('/servercheck', RequestMethod.get())
+  void servercheck(Request request, Response response) {
+    var env = Platform.environment;
+    var port = env.entries.firstWhere((element) => element.key == 'PORT',
+        orElse: () => MapEntry('PORT', '8080'));
+    response.send('your server is running at port ${port.value}');
+  }
 }
